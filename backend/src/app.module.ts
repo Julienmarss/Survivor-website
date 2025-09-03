2dc0591e843bd0809d62b184fef54686e43c5b54
Julien Mars <julien.mars@epitech.eu>
2025-09-03 09:40:38 +0200

    feat: app module with the StartupsModule added and the TyopeOrmModule added too that manage the sqlite databse

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { HttpModule } from '@nestjs/axios';
 import { AppController } from './app.controller';
 import { JebApiModule } from './modules/jeb-api/jeb-api.module';
 import { StartupsModule } from './modules/startups/startups.module';
+import { Startup } from './modules/startups/entities/startup.entity';
+import { Founder } from './modules/startups/entities/founder.entity';
 
 @Module({
   imports: [
@@ -11,9 +14,16 @@ import { StartupsModule } from './modules/startups/startups.module';
       isGlobal: true,
       envFilePath: '.env',
     }),
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'database.sqlite',
+      entities: [Startup, Founder],
+      synchronize: true, // À mettre à false en production
+      logging: false,
+    }),
     HttpModule,
     JebApiModule,
-    StartupsModule, // NOUVEAU MODULE
+    StartupsModule,
   ],
   controllers: [AppController],
 })
